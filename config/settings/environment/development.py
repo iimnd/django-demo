2e2204e43b26324661db019f62a388a80e2ebd93
iimnurd <iim@alterra.id>
2021-12-27 14:49:35 +0700

    change debug to false

--- config/settings/environment/development.py
+++ config/settings/environment/development.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-#&to%qr6++8dnc(d0zfk8ybyhuw1==95joqo-16!0*u!at#wri"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
