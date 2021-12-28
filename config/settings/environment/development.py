9eeb6b55be90f4d2db775b31f5565baac40585f6
iimnurd <iim@alterra.id>
2021-12-28 16:15:05 +0700

    change deployment

--- config/settings/environment/development.py
+++ config/settings/environment/development.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-#&to%qr6++8dnc(d0zfk8ybyhuw1==95joqo-16!0*u!at#wri
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'monitoring-demo.sumpahpalapa.com', 'djangoapp-svc.architect.svc.cluster.local']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'monitoring-demo.sumpahpalapa.com']
 
 ROOT_URLCONF = "config.urls"
 
